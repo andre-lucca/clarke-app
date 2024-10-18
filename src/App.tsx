6b01b24a64022bd644c521a104792a5dceda3f8c
Andre Luca <lucaprojob@gmail.com>
2024-10-18 15:08:16 -0300

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+
+  return (
+    <div>
+      <header className="bg-gradient-to-r from-green-800 to-neutral-700 h-[20rem]">
+        <h1 className="text-center text-white">Clarke Energia - Calculadora</h1>
+      </header>
+      <div>
+        <input type="text" />
+      </div>
+    </div>
+  )
+}
+
+export default App
